698af97d2ae3263cf0581bc03dcff1fb4a89bde0
mokdevil <122613579+mokdevil@users.noreply.github.com>
2023-01-23 09:14:02 +0200

    Update file_uploader.py

--- file_uploader.py
+++ file_uploader.py
@@ -1,33 +1,27 @@
 import streamlit as st
-from io import StringIO
 from PIL import Image
 
 st.title("File uploader test")
 st.write("Choose a .py, .jpg or a .txt file")
 
 uploaded_file = st.file_uploader("Choose a file", type=["py", "jpg", "txt"])
-    
+
 if uploaded_file is not None:
-    
     filename = uploaded_file.name
 
-    pythonscript = (filename.find(".py"))
-    textfile = (filename.find(".txt"))
-    jpgfile = (filename.find(".jpg"))
-    h = 0
+    if filename.endswith(".py"):
+        with uploaded_file as f:
+            code_string = f.read().decode("utf-8")
+            if st.checkbox("Show source code"):
+                st.code(code_string, language=("python"))
+            new_code_string = code_string.replace("st.set_page_config", "#")
+            if st.checkbox("Run python script"):
+                exec(new_code_string)
 
-    if h <= pythonscript:
-        stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-        string_data = stringio.read()
-        st.code(string_data, language=("python"))
-        run_file_button = st.checkbox("Run python script")
-        if run_file_button is True:
-            new_string_data = string_data.replace("st.set_page_config", "#")
-            exec(new_string_data)
-    elif h <= textfile:
-        stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-        string_data = stringio.read()
-        st.write(string_data)
-    elif h <= jpgfile:
+    elif filename.endswith(".txt"):
+        with uploaded_file as f:
+            st.write(f.read().decode("utf-8"))
+
+    elif filename.endswith(".jpg"):
         image = Image.open(uploaded_file)
         st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
